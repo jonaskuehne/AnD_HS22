edaf206f3563291e0dda52e6ddc45f6dc8882213
Jonas Kühne <69720225+jonaskuehne@users.noreply.github.com>
2022-11-06 20:46:12 +0100

    Add files via upload

---
+++ AnD_HS22/src/ex4/CallCountA.java
@@ -0,0 +1,20 @@
+package ex4;
+
+public class CallCountA {
+    public static void main(String[] args) {
+        int count = 0;
+
+        int n = 15;
+        int i = 0;
+        while (Math.pow(2, i) < n) {
+            int j = i;
+            while (j < n) {
+                ++count;
+                System.out.println(i + " " + j + " " + count);
+                ++j;
+            }
+            ++i;
+        }
+        System.out.println(count);
+    }
+}
---
+++ AnD_HS22/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jonas
+ *
+ */
+module AnD_HS22 {
+}
